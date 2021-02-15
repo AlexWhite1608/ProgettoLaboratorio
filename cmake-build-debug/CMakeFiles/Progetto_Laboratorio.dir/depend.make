ab41d9cf490782aac748f59787d982538593b34f
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2021-02-15 11:50:48 +0100

    Inizializzazione del progetto!

---
+++ cmake-build-debug/CMakeFiles/Progetto_Laboratorio.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Progetto_Laboratorio.
+# This may be replaced when dependencies are built.
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Inizializzazione del progetto!" << std::endl;
+    return 0;
+}
