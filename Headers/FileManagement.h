24f6b665f40d560a06b972f97fe56ac441604960
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2021-02-18 12:09:35 +0100

    Initialized the FileManagement class with the main attributes

---
+++ Headers/FileManagement.h
@@ -0,0 +1,18 @@
+//
+// Created by aless on 18/02/2021.
+//
+
+#include <string>
+
+#ifndef PROGETTO_LABORATORIO_FILEMANAGEMENT_H
+#define PROGETTO_LABORATORIO_FILEMANAGEMENT_H
+
+class FileManagement{
+public:
+
+private:
+    std::string fileName;
+    std::string filePath;
+};
+
+#endif //PROGETTO_LABORATORIO_FILEMANAGEMENT_H
---
+++ Sources/FileManagement.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aless on 18/02/2021.
+//
+
+#include "../Headers/FileManagement.h"
